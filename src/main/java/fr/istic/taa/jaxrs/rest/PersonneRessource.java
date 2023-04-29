0ae7d61e6167c5ca4349d9800b96ac9828519f4b
huthomas <hugo.thomas@etudiant.univ-rennes1.fr>
2023-04-30 00:11:14 +0200

    Complétion fichier Ressource

--- src/main/java/fr/istic/taa/jaxrs/rest/PersonneRessource.java
+++ src/main/java/fr/istic/taa/jaxrs/rest/PersonneRessource.java
@@ -1,22 +1,38 @@
 package fr.istic.taa.jaxrs.rest;
 
 
+import fr.istic.taa.jaxrs.dao.EntityManagerHelper;
 import fr.istic.taa.jaxrs.dao.PersonneDAO;
 import fr.istic.taa.jaxrs.domain.Personne;
 import fr.istic.taa.jaxrs.domain.SupportMember;
 import fr.istic.taa.jaxrs.domain.User;
 import io.swagger.v3.oas.annotations.Parameter;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/personne")
 @Produces({"application/json"})
 public class PersonneRessource {
 
     @GET
+    public Response getAllPersonnes() {
+        PersonneDAO personneDAO = new PersonneDAO();
+        List<Personne> personnes = personneDAO.getAllPersonnes();
+        return Response
+                .ok(personnes)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+                .header("Access-Control-Allow-Headers", "Content-Type")
+                .build();
+    }
+
+    @GET
     @Path("/{personneId}")
-    public Personne getPersonneById(@PathParam("personneId") Long personneId) {
+    public Response getPersonneById(@PathParam("personneId") Long personneId) {
         PersonneDAO personneDAO = new PersonneDAO();
         Personne personne = personneDAO.getPersonneByID(personneId);
         String type = personne instanceof User ? "User" : "SupportMember";
@@ -25,17 +41,28 @@ public class PersonneRessource {
             User user = new User();
             user.setId_member(personne.getId_member());
             user.setName(personne.getName());
-            return user;
+            return Response
+                    .ok(user)
+                    .header("Access-Control-Allow-Origin", "*")
+                    .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+                    .header("Access-Control-Allow-Headers", "Content-Type")
+                    .build();
         } else{
             SupportMember support = new SupportMember();
             support.setId_member(personne.getId_member());
             support.setName(personne.getName());
-            return support;
+            return Response
+                    .ok(support)
+                    .header("Access-Control-Allow-Origin", "*")
+                    .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+                    .header("Access-Control-Allow-Headers", "Content-Type")
+                    .build();
         }
 
     }
 
     //curl -X POST -H 'Content-type:application/json' -H 'Accept:application/json' -d '{"name":"peter"}' http://localhost:8080/personne
+    /*
     @POST
     @Consumes("application/json")
     public Response addPersonne(
@@ -43,5 +70,22 @@ public class PersonneRessource {
         // add pet
 
         return Response.ok().entity("SUCCESS").build();
+    }*/
+    @POST
+    @Path("/addPersonne/{nom}")
+    @Consumes("application/json")
+    public Response addPersonne(
+            @PathParam("nom") String nom) {
+        // add Personne
+        PersonneDAO personneDAO = new PersonneDAO();
+        EntityManager manager = EntityManagerHelper.getEntityManager();
+        EntityTransaction tx = manager.getTransaction();
+        tx.begin();
+        User user = new User(nom);
+        personneDAO.save(user);
+        tx.commit();
+        manager.close();
+
+        return Response.ok().entity("ADD User SUCCESS : " + user).build();
     }
 }
